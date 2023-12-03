0c6a0add6e78c3cce19c6950b0f1771d2617cc3d
cbiers <chris.biersbach@gmail.com>
2023-12-03 11:28:26 +0100

    Day 2 Part 2 done

---
+++ 2/2/2_2.py
@@ -0,0 +1,19 @@
+f = open("in.txt", "r")
+lines = f.readlines()
+
+sum = 0
+
+for line in lines:
+    parts = line.split(":")
+    num = int(parts[0].split()[1])
+    games = parts[1].split(";")
+    curr = {"red": 0, "green": 0, "blue": 0}
+    for game in games:
+        colors = game.split(",")
+        for color in colors:
+            p = color.split()
+            a, c = int(p[0]), p[1]
+            curr[c] = max(curr[c], a)
+    sum += curr["red"] * curr["blue"] * curr["green"]
+
+print(sum)
